715d9ff3fba0872fd62a6d0192aef75bb09fa65d
KolesnikovAM <akolesnikov@fil-it.ru>
2022-04-13 12:12:05 +0300

    Initial commit

---
+++ src/main/java/ro/planet/documentum/stada/modules/pdf/HTMLExtractAttrs.java
@@ -0,0 +1,15 @@
+package ro.planet.documentum.stada.modules.pdf;
+
+public class HTMLExtractAttrs {
+
+    private String objectId;
+
+    public String getObjectId() {
+	return objectId;
+    }
+
+    public void setObjectId(String objectId) {
+	this.objectId = objectId;
+    }
+
+}
---
+++ src/main/java/ro/planet/documentum/stada/modules/pdf/HTMLExtractResult.java
@@ -0,0 +1,15 @@
+package ro.planet.documentum.stada.modules.pdf;
+
+public class HTMLExtractResult {
+
+    private String result;
+
+    public String getResult() {
+	return result;
+    }
+
+    public void setResult(String result) {
+	this.result = result;
+    }
+
+}
